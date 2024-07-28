86240a49a74fb296fa0c668b3b0fef26c833582e
Trent Lopata <tlopata@icloud.com>
2024-07-28 14:21:08 -0400

    quick sort is working

--- algorithms/elegant_quick_sort.py
+++ algorithms/elegant_quick_sort.py
@@ -1,19 +1,27 @@
+import random
 from typing import Iterable
 
-def quick_sort(array: Iterable) -> Iterable:
+def quick_sort(list_input: Iterable) -> Iterable:
 
-    if len(array) <= 1:
-        return array
+    if len(list_input) <= 1:
+        return list_input
 
-    pivot = array[-1]
-    smaller = list(filter(lambda x: x < pivot, array))
-    larger = list(filter(lambda x: x >= pivot, array))
+    pivot = list_input[-1]
+
+    # remove pivot from list without mutation
+    list_input = list(filter(lambda x: x != pivot, list_input))
+
+    smaller = list(filter(lambda x: x < pivot, list_input))
+    larger = list(filter(lambda x: x >= pivot, list_input))
 
     return quick_sort(smaller) + [pivot] + quick_sort(larger)
 
 
 # test statements
+RAND_LIST = [random.randint(1, 50) for _ in range(50)]
 
-print(quick_sort([5, 2, 9, ]))  # [1, 2, 5, 5, 6, 9]
+print(RAND_LIST)
+print(quick_sort(RAND_LIST)) # weird that this removed duplicates
 
-assert quick_sort([5, 2, 9]) == [2, 5, 9]
+print(sorted(RAND_LIST))
+# assert quick_sort(RAND_LIST) == sorted(RAND_LIST)
