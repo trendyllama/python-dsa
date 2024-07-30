c5110f0122912b25540521a79a4f821821d1a7e7
trendyllama <143849158+trendyllama@users.noreply.github.com>
2024-07-30 05:56:16 -0400

    refactoring

--- algorithms/sort_speed_test.py
+++ algorithms/sort_speed_test.py
@@ -3,16 +3,16 @@ import time
 from bubble_sort import bubble_sort
 from elegant_quick_sort import quick_sort
 
-nums = random.sample(range(1, 10000), 800)
+NUMS = [random.randint(1 ,800) for _ in range(1000)]
 # if multithreading, nums would need to be inside each functions local scope
 
 
 s = time.time()
-bubble_sort(nums)
+bubble_sort(NUMS)
 elapsed = time.time() - s
 print(f"Bubble sort took {elapsed} seconds")
 
 s = time.time()
-quick_sort(nums)
+quick_sort(NUMS)
 elapsed = time.time() - s
 print(f"Quick sort took {elapsed} seconds")
