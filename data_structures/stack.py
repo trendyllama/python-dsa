c8c157efe30c7ec4cd8099135c765d0d283ff3b6
trendyllama <143849158+trendyllama@users.noreply.github.com>
2024-08-10 10:57:58 -0400

    pattern matching

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -1,33 +1,61 @@
-from data_structures.node import Node
+from typing import Optional, Any
+
+
+class Node:
+    def __init__(self, value: Any, link_node = None):
+        self.value: Any = value
+        self.link_node: Optional[Node] = link_node
+
+    def set_next_node(self, link_node):
+        self.link_node = link_node
+
+    def get_next_node(self):
+        return self.link_node
+
+    def get_value(self):
+        return self.value
+
 
 
 class Stack:
     def __init__(self, name):
-        self.size = 0
-        self.top_item = None
-        self.limit = 1000
-        self.name = name
+        self.size: int = 0
+        self.top_item: Optional[Node] = None
+        self.limit: int = 1000
+        self.name: str = name
 
     def push(self, value):
-        if self.has_space():
-            item = Node(value)
-            item.set_next_node(self.top_item)
-            self.top_item = item
-            self.size += 1
-        else:
-            print("No more room!")
+        
+        match self.has_space():
+            case True:
+                item = Node(value)
+                item.set_next_node(self.top_item)
+                self.top_item = item
+                self.size += 1
+            case False:
+                print("No more room!")
 
     def pop(self):
-        if self.size > 0:
-            item_to_remove = self.top_item
-            self.top_item = item_to_remove.get_next_node()
-            self.size -= 1
-            return item_to_remove.get_value()
-        print("This stack is totally empty.")
+        
+        match self.size:
+            case self.size if self.size > 0:
+                item_to_remove = self.top_item
+                self.top_item = item_to_remove.get_next_node()
+                self.size -= 1
+                return item_to_remove.get_value()
+            case _:
+                print("This stack is  empty.")
+                return
 
     def peek(self):
-        if self.size > 0:
-            return self.top_item.get_value()
+        
+        match self.size:
+
+            case self.size if self.size > 0:
+                return self.top_item.get_value()
+            case _:
+                print('the stack does not have a top value')
+                return
 
     def has_space(self):
         return self.limit > self.size
@@ -41,11 +69,21 @@ class Stack:
     def get_name(self):
         return self.name
 
-    def print_items(self):
-        pointer = self.top_item
-        print_list = []
+    def print_items(self) -> None:
+
+        match isinstance(self.top_item, Node):
+            case True:
+                pointer = self.top_item
+            case False:
+                print("the stack is empty")
+                return
+
+        print_list: list[Node] = []
+
         while (pointer):
             print_list.append(pointer.get_value())
             pointer = pointer.get_next_node()
         print_list.reverse()
         print(f"{self.get_name()} Stack: {print_list}")
+
+        return
