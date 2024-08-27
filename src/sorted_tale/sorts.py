85642d3363e975e297c9d0ea3714b6801dc90046
trendyllama <143849158+trendyllama@users.noreply.github.com>
2024-08-27 00:50:54 +0000

    recursive bubble sort

--- src/sorted_tale/sorts.py
+++ src/sorted_tale/sorts.py
@@ -6,6 +6,9 @@ from typing import Callable, Iterable
 
 
 def bubble_sort(arr: Iterable, comparison_function: Callable) -> Iterable:
+    '''
+    - codecademy implementation of bubble sort
+    '''
     swaps = 0
 
     is_sorted = False
@@ -18,5 +21,20 @@ def bubble_sort(arr: Iterable, comparison_function: Callable) -> Iterable:
                 arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
                 swaps += 1
     print(f"Bubble sort: There were {swaps} swaps")
+
+    return arr
+
+
+def bubble_sort2(arr: Iterable, comparision_function: Callable) -> Callable | Iterable:
+    '''
+    - not correct yet
+    '''
+
+    for idx in range(len(arr) - 1):
+
+        if comparision_function(arr[idx], arr[idx + 1]):
+            arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
+
+            return bubble_sort(arr=arr, comparison_function=comparision_function)
 
     return arr
