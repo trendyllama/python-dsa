e016baf0ad7a2a7c051af09614b0218fe8de43b8
trendyllama <143849158+trendyllama@users.noreply.github.com>
2025-03-02 12:46:47 +0000

    linked list working

--- src/data_structures/linked_list.py
+++ src/data_structures/linked_list.py
@@ -2,7 +2,7 @@
 - contains linked list class
 """
 
-from typing import Any, Iterator, Optional
+from typing import Any, Optional
 
 from src.data_structures.node import Node
 
@@ -23,63 +23,36 @@ class LinkedList:
     def head_node(self, new_head_node: Optional[Node]) -> None:
         self._head_node = new_head_node
 
-    def insert(self, new_node_value: Any) -> None:
-        current_node = self.head_node
+    @property
+    def is_empty(self) -> bool:
+        return self.head_node is None
 
-        if current_node is None:
-            self.head_node = Node(new_node_value)
+    def append(self, new_node_value: Any) -> None:
+        if self.is_empty:
+            self.head_node = Node(new_node_value, None)
+            return
+
+        current_node = self.head_node
 
         while current_node is not None:
-            next_node = current_node.next_node
-
-            if not next_node:
-                current_node.next_node = new_node_value
-            current_node = next_node
-
-    def delete(self, value_to_delete: Any) -> None:
-        if self.head_node is None:
-            raise ValueError("List is empty")
-
-        current_node = self.head_node
-
-        if current_node.value == value_to_delete:
-            self.head_node = current_node.next_node
-
-        while current_node:
-            next_node = current_node.next_node
-
-            if next_node is None:
-                break
-
-            if next_node.value == value_to_delete:
-                current_node.next_node = next_node.next_node
-                current_node = None
-            else:
-                current_node = next_node
-
-    def stringify_list(self) -> str:
-        current_node = self.head_node
-        result = []
-
-        while current_node:
-            result.append(current_node.value)
+            if current_node.next_node is None:
+                current_node.next_node = Node(new_node_value, None)
+                return
             current_node = current_node.next_node
 
-        return " -> ".join(result)
+    def delete(self, value_to_delete: Any) -> None:
+        if self.is_empty:
+            return
 
-    def __str__(self) -> str:
-        return self.stringify_list()
+        current_node = self.head_node
 
-    def __iter__(self) -> Iterator[Any]:
-        self.current_node = self.head_node
+        while current_node is not None:
+            if current_node.next_node is None:
+                raise ValueError(f"Value {value_to_delete} not found in list")
 
-        return self
+            # want to look ahead by one node to see if the next node is the one to delete
+            if current_node.next_node.value == value_to_delete:
+                current_node.next_node = current_node.next_node.next_node
+                return
 
-    def __next__(self) -> Any:
-        if self.current_node is None:
-            raise StopIteration
-
-        value = self.current_node.value
-        self.current_node.next_node
-
-        return value
+            current_node = current_node.next_node
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,10 +1,11 @@
 from src.data_structures.linked_list import LinkedList
 from src.data_structures.node import Node
+import pytest
 
 
 def test_insert_empty_list():
     linked_list = LinkedList()
-    linked_list.insert(10)
+    linked_list.append(10)
 
     assert linked_list.head_node is not None
     assert linked_list.head_node.value == 10
@@ -12,41 +13,56 @@ def test_insert_empty_list():
 
 def test_insert_non_empty_list():
     linked_list = LinkedList(Node(10))
-    linked_list.insert(20)
+    linked_list.append(20)
 
     assert linked_list.head_node is not None
     assert linked_list.head_node.value == 10
     assert linked_list.head_node.next_node is not None
-    assert linked_list.head_node.next_node == 20
-
-
-def test_iterate_list():
-    linked_list = LinkedList(Node(10))
-    linked_list.insert(20)
-    linked_list.insert(30)
-    values = [value for value in linked_list]
-
-    assert all(isinstance(value, Node) for value in values)
-    assert values == [10, 20, 30]
+    assert linked_list.head_node.next_node.value == 20
 
 
 def test_insert_multiple_nodes():
     linked_list = LinkedList()
-    linked_list.insert(10)
-    linked_list.insert(20)
-    linked_list.insert(30)
+    linked_list.append(10)
+    linked_list.append(20)
+    linked_list.append(30)
 
-    assert linked_list.head_node is not None
-    assert linked_list.head_node.value == 10
+    head = linked_list.head_node
+    assert head is not None
+    assert head.value == 10
 
-    assert linked_list.head_node.next_node is not None
-    assert linked_list.head_node.next_node.value == 20
+    after_head = head.next_node
+    assert after_head is not None
+    assert after_head.value == 20
 
-    assert linked_list.head_node.next_node.next_node is not None
-    assert linked_list.head_node.next_node.next_node.value == 30
+    after_after_head = after_head.next_node
+    assert after_after_head is not None
+    assert after_after_head.value == 30
 
 
-def test_iterate_empty_list():
+def test_larger_lists():
     linked_list = LinkedList()
-    values = [value for value in linked_list]
-    assert values == []
+    for i in range(1, 100):
+        linked_list.append(i)
+        assert linked_list.head_node is not None
+        assert linked_list.head_node.value == 1
+
+    current_node = linked_list.head_node
+    for i in range(1, 100):
+        assert current_node is not None
+        assert current_node.value == i
+        current_node = current_node.next_node
+
+
+def test_delete_empty_list():
+    linked_list = LinkedList(Node(20))
+
+    linked_list.append(30)
+
+    with pytest.raises(ValueError):
+        linked_list.delete(10)
+
+    linked_list.append(10)
+
+    linked_list.delete(10)
+    assert linked_list.head_node is not None
