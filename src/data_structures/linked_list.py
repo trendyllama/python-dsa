a8aabf46c62aa53054217de7b7784cad948f6a44
trendyllama <143849158+trendyllama@users.noreply.github.com>
2025-05-04 19:17:21 +0000

    adding an insert method for linked list

--- src/data_structures/linked_list.py
+++ src/data_structures/linked_list.py
@@ -87,3 +87,121 @@ class LinkedList:
                 return
 
             current_node = current_node.next_node
+
+    def insert(self, new_node_value, index) -> None:
+        """
+        - inserts a new node at the given index
+        - if the index is out of bounds, it raises an IndexError
+
+        Examples:
+        >>> linked_list = LinkedList()
+        >>> linked_list.append(1)
+        >>> linked_list.append(2)
+        >>> linked_list.append(3)
+        >>> linked_list.insert(4, 1)
+        >>> linked_list.head_node.value
+        1
+        >>> linked_list.head_node.next_node.value
+        4
+        """
+        if self.is_empty:
+            raise IndexError("Index out of bounds")
+
+        current_node = self.head_node
+        current_index = 0
+
+        for node in self:
+
+            if current_node is None:
+                raise IndexError("Index out of bounds")
+
+            if current_index == index - 1:
+                new_node = Node(new_node_value, current_node)
+                new_node.next_node = current_node.next_node
+                current_node.next_node = new_node
+                return
+
+            current_index += 1
+            current_node = current_node.next_node
+
+
+    def __str__(self) -> str:
+        """
+        - returns a string representation of the linked list
+
+        Examples:
+        >>> linked_list = LinkedList()
+        >>> linked_list.append(1)
+        >>> linked_list.append(2)
+        >>> linked_list.append(3)
+        >>> str(linked_list)
+        '1 -> 2 -> 3'
+        """
+        if self.is_empty:
+            return "Empty list"
+
+        current_node = self.head_node
+        result = ""
+
+        while current_node is not None:
+            result += str(current_node.value) + " -> "
+            current_node = current_node.next_node
+
+        return result[:-4]
+
+    def __repr__(self) -> str:
+        """
+        - returns a string representation of the linked list
+
+        Examples:
+        >>> linked_list = LinkedList()
+        >>> linked_list.append(1)
+        >>> linked_list.append(2)
+        >>> linked_list.append(3)
+        >>> repr(linked_list)
+        '1 -> 2 -> 3'
+        """
+        return self.__str__()
+
+    def __len__(self) -> int:
+        """
+        - returns the length of the linked list
+
+        Examples:
+        >>> linked_list = LinkedList()
+        >>> linked_list.append(1)
+        >>> linked_list.append(2)
+        >>> linked_list.append(3)
+        >>> len(linked_list)
+        3
+        """
+        if self.is_empty:
+            return 0
+
+        current_node = self.head_node
+        count = 0
+
+        while current_node is not None:
+            count += 1
+            current_node = current_node.next_node
+
+        return count
+    def __iter__(self):
+        """
+        - returns an iterator for the linked list
+        - allows for iteration over the linked list
+        - used in for loops
+        - returns the value of the current node
+        """
+        self.current_node = self.head_node
+        return self
+    def __next__(self):
+        """
+        - returns the next value in the linked list
+        - raises StopIteration when the end of the linked list is reached
+        """
+        if self.current_node is None:
+            raise StopIteration
+        value = self.current_node.value
+        self.current_node = self.current_node.next_node
+        return value
