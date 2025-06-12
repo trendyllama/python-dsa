a2582b3553aeff490f6ab44c08ed2891662ded5d
trendyllama <agog.pretzel-0o@icloud.com>
2025-06-12 22:03:04 +0000

    Add PyArrow and Iris dataset display functionality

---
+++ src/pandas/arrow.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pyarrow as pa
+import pandas as pd
+from functools import wraps
+import time
+
+from .iris import display_iris, display_imdb_reviews
+
+def perf_timer(func):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+
+        s = time.perf_counter()
+        result = func(*args, **kwargs)
+        e = time.perf_counter()
+        print(f"Function {func.__name__} took {e - s:.4f} seconds to run.")
+        return result
+    return wrapper
+
+@perf_timer
+def display_pyarrow():
+
+    arrow_table = pa.table([np.random.rand(1000, 3)], names=["A", "B", "C"])
+
+    print("PyArrow table:")
+    print(arrow_table)
+
+
+@perf_timer
+def display_pandas():
+
+    # Create a Pandas DataFrame from the NumPy array
+    df = pd.DataFrame([np.random.randint(0, 100, 1000),
+                       np.random.randint(0, 100, 1000),
+                       np.random.rand(0, 100, 1000)], columns=["A", "B", "C"])
+    print(df.dtypes)
+
+    print(df)
+
+
+def main():
+    # display_pyarrow()
+    # display_pandas()
+
+    display_iris()
+
+    display_imdb_reviews()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/pandas/iris.py
@@ -0,0 +1,54 @@
+import time
+import pandas as pd
+import io
+import requests
+
+def display_iris():
+    # Load the iris dataset
+
+    url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/refs/heads/master/iris.csv"
+
+    request = requests.get(url)
+
+    if request.status_code == 200:
+
+        # Read the CSV file into a DataFrame
+
+        s = time.perf_counter()
+        df = pd.read_csv(io.BytesIO(request.content))
+
+        e = time.perf_counter()
+        print(f"Function took {e - s:.4f} seconds to run.")
+        return df
+
+
+def test_display_iris():
+    # Test the display_iris function
+    df = display_iris()
+
+    assert df is not None
+    # Check if the DataFrame is not empty
+    assert not df.empty, "DataFrame is empty"
+    # Check if the DataFrame has the expected number of columns
+    assert df.shape[1] == 5, "DataFrame does not have 5 columns"
+    # Check if the DataFrame has the expected column names
+    assert list(df.columns) == ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"], "DataFrame does not have the expected column names"
+
+
+def display_imdb_reviews():
+
+    url = "https://raw.githubusercontent.com/SkyThonk/Movie-Reviews-Sentiment-Analysis/refs/heads/master/dataset/IMDB%20Dataset.csv"
+
+    r = requests.get(url)
+
+    if r.status_code == 200:
+
+        content = io.BytesIO(r.content)
+
+        s = time.perf_counter()
+        _ = pd.read_csv(content, encoding="utf-8", engine='pyarrow')
+        print(f"Function took {time.perf_counter() - s:.4f} seconds to run.")
+
+        # s = time.perf_counter()
+        # df = pd.read_csv(content)
+        # print(f"Function took {time.perf_counter() - s:.4f} seconds to run.")
