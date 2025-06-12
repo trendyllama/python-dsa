357abcebdd4227efe651677e3c1cb92a4ebaacc3
trendyllama <agog.pretzel-0o@icloud.com>
2025-06-12 09:25:59 +0000

    remove glbally executable code from classes codecademy project

--- src/codecademy/classes.py
+++ src/codecademy/classes.py
@@ -47,85 +47,91 @@ class Business:
         self.franchises = franchises
 
 
-# Brunch
-brunch_items = {
-    "pancakes": 7.50,
-    "waffles": 9.00,
-    "burger": 11.00,
-    "home fries": 4.50,
-    "coffee": 1.50,
-    "espresso": 3.00,
-    "tea": 1.00,
-    "mimosa": 10.50,
-    "orange juice": 3.50,
-}
+def main():
+    # Brunch
+    brunch_items = {
+        "pancakes": 7.50,
+        "waffles": 9.00,
+        "burger": 11.00,
+        "home fries": 4.50,
+        "coffee": 1.50,
+        "espresso": 3.00,
+        "tea": 1.00,
+        "mimosa": 10.50,
+        "orange juice": 3.50,
+    }
 
-brunch_menu = Menu("Brunch", brunch_items, 1100, 1600)
+    brunch_menu = Menu("Brunch", brunch_items, 1100, 1600)
 
-print(brunch_menu.calculate_bill(["pancakes", "home fries", "coffee"]))
+    print(brunch_menu.calculate_bill(["pancakes", "home fries", "coffee"]))
 
-# Early Bird
-early_bird_items = {
-    "salumeria plate": 8.00,
-    "salad and breadsticks (serves 2, no refills)": 14.00,
-    "pizza with quattro formaggi": 9.00,
-    "duck ragu": 17.50,
-    "mushroom ravioli (vegan)": 13.50,
-    "coffee": 1.50,
-    "espresso": 3.00,
-}
+    # Early Bird
+    early_bird_items = {
+        "salumeria plate": 8.00,
+        "salad and breadsticks (serves 2, no refills)": 14.00,
+        "pizza with quattro formaggi": 9.00,
+        "duck ragu": 17.50,
+        "mushroom ravioli (vegan)": 13.50,
+        "coffee": 1.50,
+        "espresso": 3.00,
+    }
 
-early_bird_menu = Menu("Early Bird", early_bird_items, 1500, 1800)
+    early_bird_menu = Menu("Early Bird", early_bird_items, 1500, 1800)
 
-print(early_bird_menu.calculate_bill(["salumeria plate", "mushroom ravioli (vegan)"]))
+    print(early_bird_menu.calculate_bill(["salumeria plate", "mushroom ravioli (vegan)"]))
 
-# Dinner
-dinner_items = {
-    "crostini with eggplant caponata": 13.00,
-    "ceaser salad": 16.00,
-    "pizza with quattro formaggi": 11.00,
-    "duck ragu": 19.50,
-    "mushroom ravioli (vegan)": 13.50,
-    "coffee": 2.00,
-    "espresso": 3.00,
-}
+    # Dinner
+    dinner_items = {
+        "crostini with eggplant caponata": 13.00,
+        "ceaser salad": 16.00,
+        "pizza with quattro formaggi": 11.00,
+        "duck ragu": 19.50,
+        "mushroom ravioli (vegan)": 13.50,
+        "coffee": 2.00,
+        "espresso": 3.00,
+    }
 
-dinner_menu = Menu("Dinner", dinner_items, 1700, 2300)
+    dinner_menu = Menu("Dinner", dinner_items, 1700, 2300)
 
-# Kids
-kids_items = {
-    "chicken nuggets": 6.50,
-    "fusilli with wild mushrooms": 12.00,
-    "apple juice": 3.00,
-}
+    # Kids
+    kids_items = {
+        "chicken nuggets": 6.50,
+        "fusilli with wild mushrooms": 12.00,
+        "apple juice": 3.00,
+    }
 
-kids_menu = Menu("Kids", kids_items, 1100, 2100)
+    kids_menu = Menu("Kids", kids_items, 1100, 2100)
 
-# Take a' Arepa Menue
-arepas_menu = {
-    "arepa pabellon": 7.00,
-    "pernil arepa": 8.50,
-    "guayanes arepa": 8.00,
-    "jamon arepa": 7.50,
-}
+    # Take a' Arepa Menue
+    arepas_menu = {
+        "arepa pabellon": 7.00,
+        "pernil arepa": 8.50,
+        "guayanes arepa": 8.00,
+        "jamon arepa": 7.50,
+    }
 
-# Flagship Store
-flagship_store = Franchise(
-    "1232 West End Road", [brunch_menu, early_bird_menu, dinner_menu, kids_menu]
-)
+    # Flagship Store
+    flagship_store = Franchise(
+        "1232 West End Road", [brunch_menu, early_bird_menu, dinner_menu, kids_menu]
+    )
 
-new_installment = Franchise(
-    "12 East Mulberry Street", [brunch_menu, early_bird_menu, dinner_menu, kids_menu]
-)
+    new_installment = Franchise(
+        "12 East Mulberry Street", [brunch_menu, early_bird_menu, dinner_menu, kids_menu]
+    )
 
-arepas_place = Franchise("189 Fitzgerald Avenue", arepas_menu)
+    arepas_place = Franchise("189 Fitzgerald Avenue", arepas_menu)
 
-# print(flagship_store.available_menus(1900))
+    # print(flagship_store.available_menus(1900))
 
-# Basta Fazoolin Business
-Basta = Business("Basta Fazoolin' with my Heart", [flagship_store, new_installment])
+    # Basta Fazoolin Business
+    Basta = Business("Basta Fazoolin' with my Heart", [flagship_store, new_installment])
 
-# Take a' Arepa Business
-Arepa = Business("Take a' Arepa", arepas_place)
+    print(Basta.franchises)
 
-# print(Arepa.franchises)
+    # Take a' Arepa Business
+    Arepa = Business("Take a' Arepa", arepas_place)
+
+    print(Arepa.franchises)
+
+if __name__ == "__main__":
+    main()
