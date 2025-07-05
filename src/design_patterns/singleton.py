bfa54b7357c7673a035fd835dd9325e0f14472a4
trendyllama <agog.pretzel-0o@icloud.com>
2025-07-05 14:36:51 +0000

    Add SingletonMeta metaclass for singleton class implementation

--- src/design_patterns/singleton.py
+++ src/design_patterns/singleton.py
@@ -1,6 +1,21 @@
 import os
 
 
+class SingletonMeta(type):
+    """
+    A metaclass for creating singleton classes.
+    Ensures that only one instance of the class can exist.
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
 class Configuration:
     def __new__(cls):
         if not hasattr(Configuration, "instance"):
