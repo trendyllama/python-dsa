f45c828d3452f3e894c9134a96d657bc92480b07
trendyllama <agog.pretzel-0o@icloud.com>
2025-07-20 08:39:03 +0000

    Replace time.time() with time.perf_counter() for more accurate runtime measurement in the measure_running_time decorator

--- src/codecademy/measure_runtime.py
+++ src/codecademy/measure_runtime.py
@@ -7,9 +7,9 @@ def measure_running_time(echo=True):
     def decorator(func):
         @functools.wraps(func)
         def wrapped(*args, **kwargs):
-            t_1 = time.time()
+            t_1 = time.perf_counter()
             ans = func(*args, **kwargs)
-            t_2 = time.time()
+            t_2 = time.perf_counter()
             if echo:
                 print(f"{func.__name__}() running time is {t_2 - t_1:.2f} s")
             return ans
