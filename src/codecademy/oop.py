e8f343e327080a56962e6468550a2c36d599246e
trendyllama <agog.pretzel-0o@icloud.com>
2025-07-20 08:36:47 +0000

    Enhance docstrings for School, PrimarySchool, and HighSchool classes with examples and remove unused getter methods

--- src/codecademy/oop.py
+++ src/codecademy/oop.py
@@ -1,34 +1,38 @@
 class School:
+    """
+
+    Example:
+    >>> s = School("Hill", "High", 100)
+    >>> s.name
+    'Hill'
+    >>> s.level
+    'High'
+    >>> s.numberofStudents
+    100
+    >>> s.__repr__()
+    'A High school named Hill with 100 students'
+    """
+
     def __init__(self, name, level, numberofStudents):
         self.name = name
         self.level = level
         self.numberofStudents = numberofStudents
 
-    def get_name(self):
-        return self.name
-
-    def get_level(self):
-        return self.level
-
-    def get_numberofStudents(self):
-        return self.numberofStudents
-
-    def set_numberofStudents(self, num):
-        self.numberofStudents = num
-
     def __repr__(self):
-        return (
-            "A "
-            + str(self.level)
-            + " school named "
-            + str(self.name)
-            + " with "
-            + str(self.numberofStudents)
-            + " students"
-        )
+        return f"A {self.level} school named {self.name} with {self.numberofStudents} students"
 
 
 class PrimarySchool(School):
+    '''
+    Example:
+    >>> p = PrimarySchool("Wyndcroft", 500, "Parents only")
+    >>> p.name
+    'Wyndcroft'
+    >>> p.level
+    'primary'
+    >>> p.numberofStudents
+    500
+    '''
     def __init__(self, name, numberofStudents, pickupPolicy):
         super().__init__(name, "primary", numberofStudents)
         self.pickupPolicy = pickupPolicy
@@ -41,6 +45,21 @@ class PrimarySchool(School):
 
 
 class HighSchool(School):
+    '''
+
+    Example:
+    >>> h = HighSchool("Lawrenceville", 100, ["Soccer", "Track", "Hockey"])
+    >>> h.name
+    'Lawrenceville'
+    >>> h.level
+    'High'
+    >>> h.numberofStudents
+    100
+    >>> h.sportsTeams
+    ['Soccer', 'Track', 'Hockey']
+    >>> h.__repr__()
+    "We offer ['Soccer', 'Track', 'Hockey'] as extra ciricular sports"
+    '''
     def __init__(self, name, numberofStudents, sportsTeams):
         super().__init__(name, "High", numberofStudents)
         self.sportsTeams = sportsTeams
@@ -50,9 +69,3 @@ class HighSchool(School):
 
     def __repr__(self):
         return "We offer {} as extra ciricular sports".format(self.sportsTeams)
-
-
-if __name__ == "__main__":
-    p = PrimarySchool("Wyndcroft", 500, "Parents only")
-    h = HighSchool("Lawrenceville", 100, ["Soccer", "Track", "Hockey"])
-    s = School("Hill", "High", 100)
