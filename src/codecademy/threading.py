33990f2c4f87eb90b406a6335a210b7e736638a3
trendyllama <agog.pretzel-0o@icloud.com>
2025-07-20 08:39:10 +0000

    Refactor docstring formatting for clarity and consistency in threading.py

--- src/codecademy/threading.py
+++ src/codecademy/threading.py
@@ -1,19 +1,20 @@
 """
-Multithreading is when you can execute multiple locally contained processes simultaneously.
+- Multithreading is when you can execute multiple locally contained processes simultaneously.
+- Different functions (processes) are defined in their own scope before the main thread.
+- In python, there is not true multithreading but simulated multithreading via the threading module.
+- To calculate time.
 
-Different functions (processes) are defined in their own scope before the main thread.
-
-In python, there is not true multithreading but simulated multithreading via the threading module.
-
-To calculate time.
-
+```python
 import time
-  s = time.perf_counter()
-  elapsed = time.perf_counter() - s
-  print("Executed in: " + str(elapsed) + " seconds")
+
+s = time.perf_counter()
+elapsed = time.perf_counter() - s
+print("Executed in: " + str(elapsed) + " seconds")
+```
 
 """
 
+import asyncio
 import threading
 import os
 
@@ -26,6 +27,16 @@ def task1():
 def task2():
     print("Task 2 assigned to thread: {}".format(threading.current_thread().name))
     print("ID of process running task 2: {}".format(os.getpid()))
+
+
+async def tasks_example() -> None:
+    task1_thread = asyncio.to_thread(task1)
+    task2_thread = asyncio.to_thread(task2)
+
+    res1, res2 = await asyncio.gather(task1_thread, task2_thread)
+
+    print("Task 1 result: {}".format(res1))
+    print("Task 2 result: {}".format(res2))
 
 
 if __name__ == "__main__":
