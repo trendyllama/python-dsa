4e3212e7de54a67fe2cfbc99a99df8101a9d683d
trendyllama <agog.pretzel-0o@icloud.com>
2025-07-20 08:37:25 +0000

    Add examples to docstring of larger_sum function for clarity

--- src/codecademy/which_list_is_larger.py
+++ src/codecademy/which_list_is_larger.py
@@ -1,8 +1,16 @@
 from functools import reduce
-import random
 
 
 def larger_sum(lst1: list[int], lst2: list[int]) -> tuple[int, list[int], int]:
+    '''
+    Example:
+    >>> larger_sum([1, 2, 3], [4, 5])
+    (2, [4, 5], 9)
+    >>> larger_sum([10, 20, 30], [5, 15])
+    (1, [10, 20, 30], 60)
+    >>> larger_sum([1, 2], [3, 4, 5])
+    (2, [3, 4, 5], 12)
+    '''
     sum1 = reduce(lambda x, y: x + y, lst1)
 
     sum2 = reduce(lambda x, y: x + y, lst2)
@@ -12,9 +20,3 @@ def larger_sum(lst1: list[int], lst2: list[int]) -> tuple[int, list[int], int]:
         lst1 if sum1 > sum2 else lst2,
         sum1 if sum1 > sum2 else sum2,
     )
-
-
-if __name__ == "__main__":
-    lst_first = list(map(lambda _: random.randint(0, 1000), range(200)))
-    lst_second = list(map(lambda _: random.randint(0, 1000), range(200)))
-    print(larger_sum(lst_first, lst_second))
