64d0e8d422f1ccaaecbafc2b233fce2ee0d50473
trendyllama <agog.pretzel-0o@icloud.com>
2025-08-06 22:47:39 +0000

    add function to load books into sqlite db

--- src/sorted_tale/utils.py
+++ src/sorted_tale/utils.py
@@ -5,6 +5,7 @@
 import csv
 from pathlib import Path
 from dataclasses import dataclass
+import sqlite3
 
 
 @dataclass
@@ -42,3 +43,45 @@ def load_books(filename: Path) -> list[Book]:
         bookshelf.append(book_temp)
 
     return bookshelf
+
+
+def load_books_to_db(filename: Path) -> None:
+    """
+    This code loads the current book shelf data from the csv file
+    and saves it to the database
+
+    """
+
+    with (
+        sqlite3.connect("books.db") as conn,
+        open(filename, "r", encoding="utf-8") as file,
+    ):
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS books (
+                title TEXT,
+                author TEXT,
+                author_lower TEXT,
+                title_lower TEXT
+            )
+            """
+        )
+
+        shelf = csv.DictReader(file)
+
+        for book in shelf:
+            cursor.execute(
+                """
+                INSERT INTO books (title, author, author_lower, title_lower)
+                VALUES (?, ?, ?, ?)
+                """,
+                (
+                    book["title"],
+                    book["author"],
+                    book["author"].lower(),
+                    book["title"].lower(),
+                ),
+            )
+
+        conn.commit()
