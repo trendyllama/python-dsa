d9b5ebaf95d28d624c7bfbb8fe2d12e559576516
trendyllama <agog.pretzel-0o@icloud.com>
2025-08-09 23:49:42 +0000

    add another sql example

--- src/pandas/polars_test.py
+++ src/pandas/polars_test.py
@@ -3,6 +3,7 @@
 import io
 import polars as pl
 import requests
+import numpy as np
 
 url = "https://raw.githubusercontent.com/SkyThonk/Movie-Reviews-Sentiment-Analysis/refs/heads/master/dataset/IMDB%20Dataset.csv"
 
@@ -13,4 +14,7 @@ if r.status_code != 200:
 
 df = pl.read_csv(io.BytesIO(r.content), encoding="utf-8").sql("SELECT * FROM self where sentiment = 'negative'")
 
+df = pl.DataFrame(np.random.rand(100000, 16)).sql("SELECT column_0, column_4 FROM self WHERE column_0 > 0.9 and column_4 < 0.1")
+
 print(df)
+print(df.describe())
